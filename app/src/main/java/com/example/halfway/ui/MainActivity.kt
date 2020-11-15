e65412ef3fa061f3dc4ed4df344ae3ee4beb7274
Pavan Kumar <pavan1995p@gmail.com>
2020-11-15 13:48:29 +0530

    FunFacts Project Starter

--- app/src/main/java/com/example/halfway/ui/MainActivity.kt
+++ app/src/main/java/com/example/halfway/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.halfway.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.RecyclerView
 import com.example.halfway.R
@@ -13,18 +14,25 @@ class MainActivity : AppCompatActivity() {
     lateinit var appBarLayout: AppBarLayout
     lateinit var rcv_datalist: RecyclerView
     lateinit var mainViewModel: MainViewModel
-
+    lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         init()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+
     }
 
     private fun init() {
 
         mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         appBarLayout = findViewById(R.id.appBarLayout)
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.title = localClassName
         rcv_datalist = findViewById(R.id.rcv_datalist)
     }
 }
